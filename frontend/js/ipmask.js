906e2a564590ac69f33cdb3f9f851e8a089095b7
ochaplashkin <olegchaplashkin@gmail.com>
2019-04-23 15:24:44 +0400

    testfix

--- frontend/js/ipmask.js
+++ frontend/js/ipmask.js
@@ -1,10 +1,11 @@
     $( document ).ready(function() {
         $('#example').ipmask();
     });
+/*
   $( document ).ready(function() {
         $('#ipInfoNext').ipmask();
 });
-
+*/
 $.fn.setCursorPosition = function(pos) {
     this.each(function(index, elem) {
         if (elem.setSelectionRange) {
